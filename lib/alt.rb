5ded555c889eb32a0963f99111df8a555359020f
Sam Aarons <samaarons@gmail.com>
2012-02-09 18:01:03 -0500

    Add some test support

--- lib/alt.rb
+++ lib/alt.rb
@@ -1,8 +1,23 @@
+require "rubygems"
+require "bundler/setup"
+
+require "kpeg"
+
 require "alt/version"
 require "alt/operator"
 require "alt/precedence"
 
-require "grammar.kpeg.rb"
+begin
+  require "grammar.kpeg.rb"
+rescue LoadError
+  KPeg.load File.expand_path("../grammar.kpeg", __FILE__), "Alt::Parser"
+end
 
 module Alt
+  def self.ast(program)
+    parser = Parser.new(program)
+    if parser.parse
+      return parser.result
+    end
+  end
 end
