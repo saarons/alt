4e6de11ecde403219348322bb337f2129b1661b5
Sam Aarons <samaarons@gmail.com>
2012-02-19 16:27:59 -0500

    Add objects

---
+++ lib/alt/ast/object_literal.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+
+require "alt/object"
+
+class Alt::AST::ObjectLiteral
+  def eval(context)
+    Alt::Object.new(@object.reduce({}) { |memo, (k, v)| memo.merge(k => v.eval(context)) })
+  end
+end
---
+++ lib/alt/object.rb
@@ -0,0 +1,9 @@
+# encoding: UTF-8
+
+require "alt/value"
+
+class Alt::Object < Alt::Value
+  def initialize(object)
+    @value = object
+  end
+end
