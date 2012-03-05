74056f7aca157ba1def862dfe3b37430d6f80288
Sam Aarons <samaarons@gmail.com>
2012-03-05 15:12:20 -0500

    Remove explicit self

--- lib/alt/core_ext/array.rb
+++ lib/alt/core_ext/array.rb
@@ -6,13 +6,13 @@ require "alt/booleans"
 class Array
   def eval(context)
     return_value = nil
-    self.each do |node|
+    each do |node|
       return_value = node.eval(context)
     end
     return_value || Alt::Nil.instance
   end
   
   def to_alt
-    Alt::Array.new(self.map { |o| o.to_alt })
+    Alt::Array.new(map { |o| o.to_alt })
   end
 end
