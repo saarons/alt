565d8301a3b107988450b8337631543efe3232a7
Sam Aarons <samaarons@gmail.com>
2012-03-05 14:40:07 -0500

    add Array#length

--- lib/alt/array.rb
+++ lib/alt/array.rb
@@ -3,11 +3,17 @@
 require "alt/value"
 
 class Alt::Array < Alt::Value
+  attr_reader :value
+  
   def initialize(array = [])
     @value = array
   end
   
   def inspect
     "[" + @value.map(&:inspect).join(", ") + "]"
+  end
+  
+  attribute("length") do |receiver|
+    receiver.value.length.to_alt
   end
 end
