3607ba877aeadd4e3e7cd5f4a5b63ceac01d4835
Sam Aarons <samaarons@gmail.com>
2012-03-05 13:38:19 -0500

    Change hash syntax

--- lib/alt/object.rb
+++ lib/alt/object.rb
@@ -6,4 +6,8 @@ class Alt::Object < Alt::Value
   def initialize(object = {})
     @value = object
   end
+  
+  def inspect
+    "{#{@value.map { |k, v| k.inspect + ": " + v.inspect }.join(", ")}}"
+  end
 end
