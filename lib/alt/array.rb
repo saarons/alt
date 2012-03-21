ecd9da660546523855430c9ed28dc94288307ad4
Ethan Nam <sungju.nam@gmail.com>
2012-03-21 01:39:43 -0400

    Added the array methods.

--- lib/alt/array.rb
+++ lib/alt/array.rb
@@ -9,6 +9,37 @@ class Alt::Array < Alt::Value
     @value = array
   end
   
+  method("concat") do |receiver, argument|
+    s1 = receiver.value
+    s2 = argument.value
+    s1.concat(s2)
+  end
+  
+  method("equals") do |receiver, argument|
+    s1 = receiver.value
+    s2 = argument.value
+    (s1 == s2).to_alt
+  end
+  
+  method("pop") do |receiver|
+    s = receiver.value
+    if not s.empty?
+      s.pop()
+    else
+      "The array is empty."
+    end
+  end
+  
+  method("push") do |receiver, argument|
+    arr = receiver.value
+    arr.push(argument)
+  end
+  
+  method("size") do |receiver|
+    s = receiver.value
+    s.size()
+  end
+  
   def inspect
     "[" + @value.map(&:inspect).join(", ") + "]"
   end
