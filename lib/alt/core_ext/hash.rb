40f25c899b7ec4865db59403bdc21574102940c0
Sam Aarons <samaarons@gmail.com>
2012-03-20 20:58:28 -0400

    Fix objects

--- lib/alt/core_ext/hash.rb
+++ lib/alt/core_ext/hash.rb
@@ -4,6 +4,6 @@ require "alt/object"
 
 class Hash
   def to_alt
-    Alt::Object.new(reduce({}) { |memo, (k, v)| memo.merge(k.to_s => v.to_alt) })
+    Alt::Object.new(reduce({}) { |memo, (k, v)| memo.merge(k.to_alt => v.to_alt) })
   end
 end
