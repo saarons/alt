82f9075d88d675c995f43977ef4d6c303726e364
Sam Aarons <samaarons@gmail.com>
2012-04-02 18:58:46 -0400

    Fix string encodings

--- lib/alt/parser.rb
+++ lib/alt/parser.rb
@@ -5,5 +5,4 @@ class Alt::Parser < KPeg::CompiledParser
   QUOTE_STRING = QUOTE_REGEXP.source.gsub("|", "")
   NOT_QUOTE = Regexp.new("[^#{QUOTE_STRING}]")
   ESCAPED_QUOTE = Regexp.new("\\\\[#{QUOTE_STRING}]")
-  ESCAPE_SEQUENCES = {"\\n" => "\n", "\\b" => "\b", "\\f" => "\f", "\\r" => "\r", "\\t" => "\t", ESCAPED_QUOTE => "\\0", "\\\\" => "\\"}
 end
