8929970809f80d2daf57ce36b80346ef36f34478
Sam Aarons <samaarons@gmail.com>
2012-04-02 15:36:51 -0400

    Try to fix quotes

--- lib/alt/parser.rb
+++ lib/alt/parser.rb
@@ -2,7 +2,8 @@
 
 class Alt::Parser < KPeg::CompiledParser  
   QUOTE_REGEXP = /'|\"|\p{Initial_Punctuation}|\p{Final_Punctuation}/
-  NOT_QUOTE = Regexp.new("[^#{QUOTE_REGEXP.source}]")
-  ESCAPED_QUOTE = Regexp.new("\\\\[#{QUOTE_REGEXP.source}]")
+  QUOTE_STRING = QUOTE_REGEXP.source.gsub("|", "")
+  NOT_QUOTE = Regexp.new("[^#{QUOTE_STRING}]")
+  ESCAPED_QUOTE = Regexp.new("\\\\[#{QUOTE_STRING}]")
   ESCAPE_SEQUENCES = {"\\n" => "\n", "\\b" => "\b", "\\f" => "\f", "\\r" => "\r", "\\t" => "\t", ESCAPED_QUOTE => "\\0", "\\\\" => "\\"}
 end
