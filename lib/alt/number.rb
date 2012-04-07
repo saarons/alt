1bb8a29381d98f084151b385bbabaa85f3222153
hai lin <hl2656@columbia.edu>
2012-04-07 18:12:53 -0400

    number update and chinese test case --hai

--- lib/alt/number.rb
+++ lib/alt/number.rb
@@ -3,64 +3,90 @@
 require "alt/value"
 
 class Alt::Number < Alt::Value
-  include Comparable
-  attr_reader :value
-  
-  def initialize(value)
-    @value = Rational(value)
-  end
-  
-  ["+", "-", "*", "/"].each do |operator|
-    method(operator) do |receiver, argument|
-      receiver.value.send(operator, argument.value)
-    end
-  end
-  
-  method("^") do |receiver, argument|
-    receiver.value ** argument.value
-  end
+	include Comparable
+	attr_reader :value
 
-  ["<", "<=", ">", ">=", "==", "!=", "<=>"].each do |symbol|
-    method(symbol) do |receiver, argument|
-      receiver.send(symbol, argument)
-    end
-  end
-  
-  method("abs") do |receiver|
-    receiver.value.abs
-  end
+	def initialize(value)
+		@value = Rational(value)
+	end
 
-  method("绝对值") do |receiver|
-    receiver.value.abs
-  end
-  method("floor") do |receiver|
-    receiver.value.floor
-  end
-  method("ceil") do |receiver|
-	  receiver.value.ceil
-  end
-  method("denominator") do |receiver|
-	  receiver.value.denominator
-  end
+	["+", "-", "*", "/"].each do |operator|
+		method(operator) do |receiver, argument|
+			receiver.value.send(operator, argument.value)
+		end
+	end
 
-method("round") do |receiver|
-	receiver.value.round
-end
+	method("^") do |receiver, argument|
+		receiver.value ** argument.value
+	end
 
-  
-  method("to_s") do |receiver|
-    receiver.inspect
-  end
-  
-  def <=>(other)
-    @value <=> other.value
-  end
-  
-  def inspect
-    if @value.denominator == 1
-      @value.numerator.to_s
-    else
-      "#{@value.numerator}/#{@value.denominator}"
-    end
-  end
+	["<", "<=", ">", ">=", "==", "!=", "<=>"].each do |symbol|
+		method(symbol) do |receiver, argument|
+			receiver.send(symbol, argument)
+		end
+	end
+	method("sqrt") do |receiver|
+		Math.sqrt(receiver.value.to_f).to_r
+	end
+	method("sin") do |receiver|
+		Math.sin(receiver.value.to_f).to_r
+	end
+	method("cos") do |receiver|
+		Math.cos(receiver.value.to_f).to_r
+	end
+	method("tan") do |receiver|
+		Math.tan(receiver.value.to_f).to_r
+	end
+	method("log10") do |receiver|
+		Math.log10(receiver.value.to_f).to_r
+	end
+	method("log2") do |receiver|
+		Math.log2(receiver.value.to_f).to_r
+	end
+
+	method("abs") do |receiver|
+		receiver.value.abs
+	end
+
+	method("绝对值") do |receiver|
+		receiver.value.abs
+	end
+	method("floor") do |receiver|
+		receiver.value.floor
+	end
+	method("ceil") do |receiver|
+		receiver.value.ceil
+	end
+	method("denominator") do |receiver|
+		receiver.value.denominator
+	end
+	method("分子") do |receiver|
+		receiver.value.denominator
+	end
+
+	method("round") do |receiver|
+		receiver.value.round
+	end
+	method("showfloat") do |receiver|
+		receiver.value.to_f.to_s
+	end
+
+
+
+
+	method("to_s") do |receiver|
+		receiver.inspect
+	end
+
+	def <=>(other)
+		@value <=> other.value
+	end
+
+	def inspect
+		if @value.denominator == 1
+			@value.numerator.to_s
+		else
+			"#{@value.numerator}/#{@value.denominator}"
+		end
+	end
 end
