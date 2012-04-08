6548a6b9e0ba0037f879a811b11ea1dc5661696b
Archie Archibong <aarchibong@gmail.com>
2012-04-08 18:43:06 -0400

    little update

--- lib/alt/string.rb
+++ lib/alt/string.rb
@@ -17,7 +17,29 @@ class Alt::String < Alt::Value
     s = s.each_char.map { |c| FULLWIDTH_TO_ASCII.include?(c) ? FULLWIDTH_TO_ASCII[c] : c }.join
     Alt::Number.new(s)
   end
+
+  method("length") do |receiver, *arguments|
+    s = receiver.value
+    Alt::Number.new(s.length)
+  end
+
+  method("to_lower") do |receiver, *arguments|
+    s = receiver.value
+    Alt::String.new(s.downcase)
+  end
   
+  method("to_upper") do |receiver, *arguments|
+    s = receiver.value
+    Alt::String.new(s.upcase)
+  end
+
+  method("subString") do |receiver, *arguments|
+    s = receiver.value
+    arg_1 = arguments[0].value
+    arg_2 = arguments[1].value  
+    Alt::String.new(s[arg_1,arg_2])
+  end
+
   def inspect
     @value.inspect
   end
